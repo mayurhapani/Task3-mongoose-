897f4dcf96e580634f7489ff898d452acf67ad4d
Mayur Hapani <hapanimayuur@gmail.com>
2024-05-15 17:28:38 +0530

    delete and edit done

--- index.js
+++ index.js
@@ -6,39 +6,69 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  userModel
-    .find({})
-    .then((users) => {
-      res.render("index", { users });
-    })
-    .catch((err) => {
+let editedUser = {};
+let isId = "";
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await userModel.find({});
+    res.render("index", { users, editedUser, isId });
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+});
+
+app.post("/addUser", async (req, res) => {
+  if (isId) {
+    const { username, email, password, age, mobile } = req.body;
+    console.log(req.body, isId, req.query.id);
+    try {
+      let user = await userModel.findOneAndUpdate(
+        { _id: isId },
+        { username, email, password, age, mobile }
+      );
+      // console.log("edit", user);
+    } catch (err) {
       console.log(err);
       return false;
-    });
-});
-app.post("/addUser", (req, res) => {
+    }
+    isId = "";
+    editedUser = {};
+    return res.redirect("/");
+  }
+
   const { username, email, password, age, mobile } = req.body;
-
-  userModel.create({ username, email, password, age, mobile });
-  res.redirect("/");
+  try {
+    let user = await userModel.create({ username, email, password, age, mobile });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 });
-app.get("/deleteData", (req, res) => {
-  const id = req.query.id;
 
-  userModel
-    .findByIdAndDelete(id)
-    .then((data) => {
-      console.log("deleted user is : ", data);
-      res.redirect("/");
-    })
-    .catch((err) => {
-      console.log(err);
-      return false;
-    });
-
-  res.redirect("/");
+app.get("/deleteData", async (req, res) => {
+  try {
+    let user = await userModel.findOneAndDelete({ _id: req.query.id });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
 });
+
+app.get("/editData", async (req, res) => {
+  try {
+    editedUser = await userModel.findOne({ _id: req.query.id });
+    isId = req.query.id;
+    return res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+});
+
 app.listen(8002, (err) => {
   if (!err) console.log("Server is running on http://localhost:8002");
 });
